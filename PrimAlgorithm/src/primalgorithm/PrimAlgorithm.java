c53cc0ff78f2a1206306c281e15d66a6e2f029fc
Eryk <Eryk@192.168.1.23>
2016-02-11 18:24:44 +0100

    test commit

--- PrimAlgorithm/src/primalgorithm/PrimAlgorithm.java
+++ PrimAlgorithm/src/primalgorithm/PrimAlgorithm.java
@@ -15,7 +15,7 @@ public class PrimAlgorithm {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Test"); // test
     }
     
 }
